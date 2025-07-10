395869a01ac429af19b9f0759919895255d095d2
Patxi <3425484+Chispasgg@users.noreply.github.com>
2025-07-10 14:37:52 +0200

    minor fix

--- run.sh
+++ run.sh
@@ -2,7 +2,11 @@
 
 cd docker || { echo "❌ Error: No se pudo cambiar al directorio docker."; exit 1; }
 docker-compose down
-echo "✅ Docker detenidos"
+echo "✅ Docker generales detenidos"
 echo ""
-cd ../sakila-ai-api/scripts
+cd ../sakila-ai-api/docker
+docker-compose down
+echo "✅ Docker temporales detenidos"
+echo ""
+cd ../scripts
 ./lanzar_run.sh
